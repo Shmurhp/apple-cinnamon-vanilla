2b2bb9bb9b0edb2044e5150dec9bee2f5b525ac6
johnsecules <john.secules@gmail.com>
2018-11-11 18:34:04 -0500

    hey

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://shmurhp_user:shmurhp123@ds031912.mlab.com:31912/local_library';
+mongoose.connect(mongoDB);;
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
